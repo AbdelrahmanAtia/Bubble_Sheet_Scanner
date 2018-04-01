4e0a5658a7b496d0cc81935359ffad81cea2e9bf
abdo attia <abdelrahmanatia94@outlook.com>
2018-04-01 23:00:27 +0200

    detected the two bottom circles

--- src/ImageUtil.java
+++ src/ImageUtil.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
@@ -10,15 +11,15 @@ public class ImageUtil {
 	}
 
 	public static Mat erode(Mat input, Mat kernel) {
-		Mat outputImageMat = new Mat();
-		Imgproc.erode(input, outputImageMat, kernel);
-		return outputImageMat;
+		Mat output = new Mat();
+		Imgproc.erode(input, output, kernel);
+		return output;
 	}
 	
 	public static Mat dilate(Mat input, Mat kernel) {
-		Mat outputImageMat = new Mat();
-		Imgproc.dilate(input, outputImageMat, kernel);
-		return outputImageMat;
+		Mat output = new Mat();
+		Imgproc.dilate(input, output, kernel);
+		return output;
 	}
 
 	public static Mat getCircularKernel(int kernelSize) {
@@ -29,5 +30,35 @@ public class ImageUtil {
 						new Size(kernelWidth, kernelHeight),
 						new Point(kernelSize, kernelSize));
 	}
+	
+	public static Mat thresholdImage(Mat image) {
+		Mat output = new Mat(image.rows(), image.cols(), image.type());
+		Imgproc.threshold(image, output, 200, 255, Imgproc.THRESH_BINARY);
+		return output;
+	}
+	
+	public static Mat detectCircles(Mat image){
+		 /*convert the image into a grayscale 
+		and blur it with a filter of kernel size 3*/
+		Mat grayImage = new Mat();
+		Imgproc.cvtColor( image.clone(), grayImage , Imgproc.COLOR_BGR2GRAY);
+		Imgproc.blur(grayImage, grayImage, new Size(3,3));		
+	 	int lowThreshold = 30;
+	 	Mat circles = new Mat();
+		Imgproc.HoughCircles(grayImage, circles,Imgproc.CV_HOUGH_GRADIENT, 1, grayImage.rows()/8, 200, lowThreshold, 0, 0 );
+		return circles;
+	}
+	
+	public static ArrayList<Point> getCirclesCenters(Mat circles, int minRadius, int maxRadius, int minTopDistance) {
+		ArrayList<Point> centers = new ArrayList<Point>();
 
+		for (int i = 0; i < circles.cols(); i++) {
+			Point center = new Point(circles.get(0, i)[0], circles.get(0, i)[1]);
+			int radius = (int) Math.round(circles.get(0, i)[2]);
+			if (radius > minRadius && radius < maxRadius && center.y > minTopDistance) {
+				centers.add(center);
+			}
+		}
+		return centers;
+	}
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.imgcodecs.Imgcodecs;
 
 public class MainApp {
@@ -9,26 +12,32 @@ public class MainApp {
 
 	public static void main(String[] args) {
 
-		// read image and convert it into Matrix
-		String srcImagePath = "1.png";
-		Mat srcImageMat = Imgcodecs.imread(srcImagePath);
-
-		// create a circular structuring element
+		String imagePath = "1.png";
 		int kernelSize = 3;
-		Mat kernel = ImageUtil.getCircularKernel(kernelSize);
-
-		// erode the image
-		Mat erodedImageMat = ImageUtil.erode(srcImageMat, kernel);
-		Imgcodecs.imwrite("eroded.png", erodedImageMat);
-
-		//create a circular structuring element 
-		kernelSize = 4;
-		kernel = ImageUtil.getCircularKernel(kernelSize);
-		//Dilate the image
-		Mat dilatedImageMat = ImageUtil.dilate(erodedImageMat, kernel);
-
-		Imgcodecs.imwrite("eroded_dilated.png", dilatedImageMat);
-
+		
+		Mat image = Imgcodecs.imread(imagePath);
+		Mat kernel = ImageUtil.getCircularKernel(kernelSize); 
+		image = ImageUtil.erode(image, kernel);
+		//Imgcodecs.imwrite("eroded.png", image);
+		kernel = ImageUtil.getCircularKernel(kernelSize + 1);
+		image = ImageUtil.dilate(image, kernel);
+		//Imgcodecs.imwrite("eroded_dilated.png", image);
+		Mat circles = ImageUtil.detectCircles(image);
+		// get the two bottom circles
+		ArrayList<Point> centers = ImageUtil.getCirclesCenters(circles, 20, 60, 1430);
+		Point leftPoint = centers.get(0);
+		Point rightPoint = centers.get(1);
+		//swap circles if they are not ordered
+		if (leftPoint.x > rightPoint.x) {
+			Point temp = leftPoint;
+			leftPoint = rightPoint;
+			rightPoint = temp;
+		}
+		//System.out.println(leftPoint.toString());
+		//System.out.println(rightPoint.toString());
+		
+		double xa = leftPoint.x, ya = leftPoint.y, xb = rightPoint.x, yb = rightPoint.y;
+	
 	}
 	
 	
